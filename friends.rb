07cee5b635ae0a7c95cef839a482f105ed074b76
Amanda Williamson <aselliott4@aol.co.uk>
2019-08-01 15:17:25 +0100

    passed test for number 3

--- friends.rb
+++ friends.rb
@@ -5,3 +5,12 @@ end
 def favourite_tv_show(person)
   return person[:favourites][:tv_show]
 end
+
+def likes_to_eat(person, food_to_check)
+  for snack in person[:favourites][:snacks]
+    if snack == food_to_check
+      return true
+  end
+  end
+  return false
+end
